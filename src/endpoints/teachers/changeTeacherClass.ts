a953639d377aed8d0dd39608cb2c8089bb5bd153
MaduBitencourt <bitencourtmadu@gmail.com>
2021-06-05 19:19:15 +0200

    change teacher class and student class

--- src/endpoints/teachers/changeTeacherClass.ts
+++ src/endpoints/teachers/changeTeacherClass.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from "express"
+import connection from "../../connection"
 
 export default async function changeTeacherClass(
     req: Request,
     res: Response
-) {
+): Promise<void> {
     try {
-        res.send("implementar changeTeacherClass")
+        const id = req.params.id
+        const { classname } = req.body
+
+        console.log(req.headers["content-type"])
+
+        await connection("teacher")
+            .update({ class_id: classname }).where({ id })
+
+        res.status(201).end()
+
     } catch (error) {
-        res.status(500).send("erro em implementar changeTeacherClass")
+        res.status(500).send("erro em implementar CreateClass")
     }
 }
